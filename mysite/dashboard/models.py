565496c5f29b886c6fd216ffd2d7159f1a4bcef3
ShannonBulloch <shannon.from.hawaii@gmail.com>
2016-11-05 11:40:11 -0700

    first db models

--- mysite/dashboard/models.py
+++ mysite/dashboard/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Badge(models.model):
+	id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User)
+	name = models.CharField()
+	image = models.CharField(blank=True, null=True)
+	create_dt = models.DateTimeField(auto_now_add=True)
--- mysite/log/models.py
+++ mysite/log/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CheckInEntry(models.model):
+	id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User)
+	feeling = models.CharField(blank=True, null=True)
+	need = models.CharField(blank=True, null=True)
+	points = models.IntegerField(default=1)
+	public = models.BooleanField(default=0)
+	create_dt = models.DateTimeField(auto_now_add=True)
+
+class JournalEntry(models.model):
+	id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User)
+	checkIn = models.ForeignKey(CheckInEntry, blank=True, null=True)
+	text = models.CharField()
+	points = models.IntegerField(default=1)
+	create_dt = models.DateTimeField(auto_now_add=True)
