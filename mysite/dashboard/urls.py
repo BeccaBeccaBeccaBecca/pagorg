c07c460eb56ba9a8a79a28e682bea53c6710ec7c
rwolfinger <rebecca@Rebeccas-MacBook-Pro-2.local>
2016-11-05 13:17:16 -0700

    updates to dashboard

--- mysite/dashboard/urls.py
+++ mysite/dashboard/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'dashboard'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^reward/(?P<reward_id>[0-9]+)/$', views.reward_detail, name='reward_detail'),
+    url(r'^journal/(?P<journal_id>[0-9]+)/$', views.journal_detail, name='journal_detail'),
 ]
--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,5 +1,42 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+
+from .models import RewardFeed, JournalEntryList
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_reward_list = RewardFeed.objects.order_by('-pub_date')[:5]
+    latest_journal_list = JournalEntryList.objects.order_by('-pub_date')[:5]
+    context = {
+    	'latest_reward_list': latest_reward_list,
+    	'latest_journal_list': latest_journal_list
+    	}
+
+    return render(request, 'dashboard/index.html', context)
+
+
+def reward_detail(request, reward_id):
+	latest_reward_list = RewardFeed.objects.order_by('-pub_date')[:5]
+	reward = RewardFeed(reward_feed_text="congrats!", pub_date=timezone.now())
+	for r in latest_reward_list:
+		if r.id == reward_id:
+			reward = r
+	
+	context = {
+    	'reward': reward
+    	}
+
+	return render(request, 'dashboard/reward.html', context)
+
+def journal_detail(request, journal_id):
+	latest_journal_list = JournalEntryList.objects.order_by('-pub_date')[:5]
+	journal = JournalEntryList(journal_entry_list_text="Today was a good day!", pub_date=timezone.now())
+	for j in latest_journal_list:
+		if j.id == journal_id:
+			journal = j
+    
+	context = {
+    	'journal': journal
+    	}
+
+	return render(request, 'dashboard/journal.html', context)
